423bb17486e52bf714f5272e9bef90c19688b48a
Shajahan <shajansheriff@gmail.com>
2022-07-08 04:01:07 +0530

    fix: lint errors

--- src/pages/api/examples.ts
+++ src/pages/api/examples.ts
@@ -2,7 +2,10 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { prisma } from "../../server/db/client";
 
-export default async (req: NextApiRequest, res: NextApiResponse) => {
+export default async function example(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
   const examples = await prisma.example.findMany();
   res.status(200).json(examples);
-};
+}
