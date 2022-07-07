4d4f15d7a77c82725d27b89a42ad8e34be01deb6
Shajahan <shajansheriff@gmail.com>
2022-07-07 11:06:30 +0530

    feat(input): add invalid & error styles

--- src/components/common/Input/Input.tsx
+++ src/components/common/Input/Input.tsx
@@ -1,26 +1,41 @@
 import clsx from "clsx";
-import { DetailedHTMLProps, InputHTMLAttributes } from "react";
+import { DetailedHTMLProps, forwardRef, InputHTMLAttributes, Ref } from "react";
 
 type Props = DetailedHTMLProps<
   InputHTMLAttributes<HTMLInputElement>,
   HTMLInputElement
->;
-export function Input({ className, ...rest }: Props) {
+> & {
+  error?: string;
+};
+export const Input = forwardRef<HTMLInputElement, Props>(function Input(
+  { className, error, ...rest },
+  ref
+) {
   return (
-    <input
-      type="text"
-      name="first-name"
-      id="first-name"
-      autoComplete="given-name"
-      className={clsx(
-        "h-10 border border-[#CBDBEA]",
-        "w-full p-4",
-        "rounded-lg",
-        "ring-primary outline-primary focus:ring-primary focus:border-primary",
-        "shadow-sm sm:text-sm",
-        className
+    <div>
+      <input
+        type="text"
+        name="first-name"
+        id="first-name"
+        autoComplete="given-name"
+        className={clsx(
+          "peer",
+          "h-10 border border-[#CBDBEA]",
+          "w-full p-4",
+          "rounded-lg",
+          "ring-primary outline-primary focus:ring-primary focus:border-primary",
+          "shadow-sm sm:text-sm",
+          "invalid:border-red-500 invalid:text-red-600  invalid:outline-red-500 focus:invalid:border-red-500 focus:invalid:ring-red-500",
+          className
+        )}
+        ref={ref}
+        {...rest}
+      />
+      {error && (
+        <p className="mt-2 invisible peer-invalid:visible text-red-500 text-sm">
+          {error}
+        </p>
       )}
-      {...rest}
-    />
+    </div>
   );
-}
+});
